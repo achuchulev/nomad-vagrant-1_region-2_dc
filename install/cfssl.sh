e602648c074c29852bb043670cfb7b9060c8fe81
achuchulev <42144722+achuchulev@users.noreply.github.com>
2019-05-27 13:44:34 +0300

    Update cfssl.sh

--- install/cfssl.sh
+++ install/cfssl.sh
@@ -8,6 +8,6 @@ for bin in cfssl cfssl-certinfo cfssljson
 do
   echo "Installing $bin..."
   curl -sSL https://pkg.cfssl.org/R1.2/${bin}_linux-amd64 > /tmp/${bin}
-  install /tmp/${bin} /usr/local/bin/${bin}
+  sudo install /tmp/${bin} /usr/local/bin/${bin}
 done
 }
